63c537195f651459f7361d7bcdc9ad2768aeaae7
jmburger <jarrydburger@gmail.com>
2018-04-11 16:42:42 +0200

    fixing added filters
    as raw values are first run through a filter before they are stored.

--- Test_code/Test_code.ino
+++ Test_code/Test_code.ino
@@ -16,7 +16,7 @@ MAX30100 sensor;
 //Heart Rate
 #define RECORDING_TIME  5000000         //Recording time for heart rate (5 seconds)
 #define SIZE  RECORDING_TIME/10000      //Vector size equal recording time divided by 10
-#define ALPHA_DCR     0.95              //DC filter alpha value
+#define ALPHA_DCR     0.75              //DC filter alpha value
 #define SAMPLE_SIZE  100                //Mean difference filter sample size used to calculate the running mean difference
 
 //Variables store raw RED and IR values
@@ -24,19 +24,17 @@ uint16_t raw_IR_Val = 0;
 uint16_t raw_RED_Val = 0;
 double IR_vec[SIZE]; //IR LED Vector for x sec
 double RED_vec[SIZE]; //IR LED Vector for x sec
-//------------------------------------------
+float Filtered_IR_vec[SIZE]; //filtered IR vector for x sec
+//-------------FILTER Variables-------------
 //DC Removal variables:
 double prev_filtered = 0;
-//------------------------------------------
 //Mean Difference variables:
 float MeanDiff_TV[SAMPLE_SIZE];     // Trailling measurements
 uint8_t Index = 0;
 uint8_t count = 0;
 float sum = 0;
-//------------------------------------------
 //Low pass filter (butterworth filter) variables:
 float Val[3];
-float BWF_output = 0;
 //------------------------------------------
 
 void setup() {
@@ -81,11 +79,16 @@ void loop() {
     sensor.update();
     if (sensor.getRawValues(&raw_IR_Val, &raw_RED_Val))
     {
-      // Serial.print(raw_IR_Val);  
+      //Serial.print(raw_IR_Val);  
+      //raw ifrared and red led values
       IR_vec[a] = raw_IR_Val;
       RED_vec[a] = raw_RED_Val;
-      // Serial.print(" | ");
-      // Serial.print(IR_vec[a]); 
+      //add filtering to raw values:
+      Filtered_IR_vec[a] = DCR_function(IR_vec[a], ALPHA_DCR);
+      Filtered_IR_vec[a] = MDF_function(Filtered_IR_vec[a]);
+      Filtered_IR_vec[a] = Butterworth_LPF_function(Filtered_IR_vec[a]);
+      //Serial.print(" | ");
+      //Serial.print(Filtered_IR_vec[a]); 
       // Serial.print(" | ");
       // Serial.println(micros()); 
       a++;
@@ -94,24 +97,14 @@ void loop() {
   }
   sensor.shutdown();
   //-----------------------Processing raw values-----------------------------------
-  double DCR_IR_vec[sizeof(IR_vec)];
-  float MDF_IR_vec[sizeof(IR_vec)];
-  float LPF_IR_vec[sizeof(IR_vec)];
-  for (int i = 0; i < sizeof(IR_vec); i++)
-    {
-      DCR_IR_vec[i] = DCR_function(IR_vec[i], ALPHA_DCR);
-      MDF_IR_vec[i] = MDF_function(DCR_IR_vec[i]);
-      LPF_IR_vec[i] = Butterworth_LPF_function(MDF_IR_vec[i]);
-    }
-
-  delay(5000);
+  //delay(5000);
   Serial.println(a);
-  for (int i = 0; i < sizeof(IR_vec); i++)
-  {
-     Serial.println(LPF_IR_vec[i]);
-  }
+  // for (int i = 0; i < sizeof(IR_vec); i++)
+  // {
+  //    Serial.println(LPF_IR_vec[i]);
+  // }
   //Serial.println("end");
-  //delay(1000);
+  delay(4000);
   sensor.resume();
 
 }
@@ -120,43 +113,38 @@ void loop() {
 //DC Removeral filter
 double DCR_function(double raw_input, float alpha) 
 {  
-  double filtered = raw_input + alpha * prev_filtered;
-  double output_DCR = filtered - prev_filtered;
-  prev_filtered = filtered;   
+  float filtered = raw_input + alpha * prev_filtered;
+  float output_DCR = filtered - prev_filtered;
+  prev_filtered = filtered;  
+  Serial.println(output_DCR); 
   return output_DCR;
 }
 // Mean difference filter
 float MDF_function(float raw_input) 
 {
   float avg = 0; 
-
   sum -= MeanDiff_TV[Index];
   MeanDiff_TV[Index] = raw_input;
   sum += MeanDiff_TV[Index];  
-
   Index++;
   Index = Index % SAMPLE_SIZE;
-
   if (count < SAMPLE_SIZE) 
   {
     count++;
   }
-
   avg = sum / count;
+  //Serial.println(avg);
   return avg - raw_input;
 }
-
 //Low pass filter (Butterworth filter)
 float Butterworth_LPF_function(float raw_input) 
 {
-
   //Second order low pass filter
   Val[0] = Val[1];
   Val[1] = Val[2];
-
   //Fs = 100Hz (sample rate) and Fc = 10Hz (cut-off frequency)
   Val[2] = (6.745527388907189559e-2 * raw_input) + (-0.41280159809618854894 * Val[0]) + (1.14298050253990091107 * Val[1]);
-  BWF_output = (Val[0] + Val[2]) + 2*Val[1];
-  
+  float BWF_output = (Val[0] + Val[2]) + 2*Val[1];
+  //Serial.println(BWF_output);  
   return BWF_output;
 }
