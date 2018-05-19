03b5b26e5c2ab5e28a45b2ca388f6675271e7668
jmburger <jarrydburger@gmail.com>
2018-05-19 12:38:11 +0200

    Add DC Current balancing
    DC currently balancing uses a new acceptable dc difference value then
    previous code.

--- Main_Code1/Main_Code1.ino
+++ Main_Code1/Main_Code1.ino
@@ -8,6 +8,56 @@
 #include <SparkFun_MLX90632_Arduino_Library.h>
 MLX90632 temp_sensor;
 
+// MAX30100 sensor
+#include <MAX30100.h>
+MAX30100 MAX30100_sensor;
+//LED currents:
+const LEDCurrent LEDCurrent_array[] = {MAX30100_LED_CURR_0MA,         //0
+                                       MAX30100_LED_CURR_4_4MA,       //1
+                                       MAX30100_LED_CURR_7_6MA,       //2
+                                       MAX30100_LED_CURR_11MA,        //3
+                                       MAX30100_LED_CURR_14_2MA,      //4
+                                       MAX30100_LED_CURR_17_4MA,      //5
+                                       MAX30100_LED_CURR_20_8MA,      //6
+                                       MAX30100_LED_CURR_24MA,        //7
+                                       MAX30100_LED_CURR_27_1MA,      //8
+                                       MAX30100_LED_CURR_30_6MA,      //9
+                                       MAX30100_LED_CURR_33_8MA,      //10
+                                       MAX30100_LED_CURR_37MA,        //11
+                                       MAX30100_LED_CURR_40_2MA,      //12
+                                       MAX30100_LED_CURR_43_6MA,      //13
+                                       MAX30100_LED_CURR_46_8MA,      //14
+                                       MAX30100_LED_CURR_50MA};       //15 
+LEDCurrent IR_current = LEDCurrent_array[15];			//define IR led current (50mA)
+LEDCurrent RED_current = LEDCurrent_array[7];			//define initial Red led current (24mA)
+#define Sample_Rate 	MAX30100_SAMPRATE_100HZ         //define sample rate (100Hz)
+#define Pulse_width     MAX30100_SPC_PW_1600US_16BITS   //define led pulse width (1600)
+#define Highres_mode    true                            //High resolution mode
+
+//Variables store raw RED and IR values
+uint16_t raw_IR_Val = 0;
+uint16_t raw_RED_Val = 0;
+//Filter Variables
+//DC Removal variables:
+#define SAMPLE_SIZE   100                 //Mean difference filter sample size used to calculate the running mean difference
+#define ALPHA_DCR    0.95                 //DC filter alpha value for LED
+bool IR_DC = false;                             //Return either DC value (true) or AC value (false)                 
+bool RED_DC = false;                            //Return either DC value (true) or AC value (false)
+double prev_filtered_IR = 0;     //HR
+double prev_filtered_RED = 0;    //Sp02
+//Mean Difference variables:
+float MeanDiff_TV[SAMPLE_SIZE];     // Trailling measurements
+uint8_t Index = 0;
+uint8_t count = 0;
+float sum = 0;
+//Low pass filter (butterworth filter) variables:
+float Val[3];
+
+//Current balancing:
+#define ACCEPTABLE_CURRENT_DIFF   2500 //Acceptable current difference between RED current and IR current
+int counter = 8;						// Current array counter
+bool Current_balaning = true;			// Set true when current balancing needs to take place. 
+
 // Timer variables:
 int time_10s = micros(); 		// time 10 seconds
 int time_60s = micros();		// time 1 minute
@@ -27,17 +77,64 @@ void setup()
 	//temp_sensor.begin();					// Start sensor
 	//temp_sensor.setMode(MODE_SLEEP);		// Put sensor in sleep mode 
 
+	// MAX30100 sensor:
+	MAX30100_sensor.begin();
+	//MAX30100_Startup();		//Start-up the MAX30100 sensor
 }
 
 void loop() 
 { 	
+	// Current balancing for Sp02 calculation:
+	// This balancing is going to take place every 5 minutes:
+	if (Current_balaning == true)
+	{
+		MAX30100_Startup();					// start-up the MAX30100 sensor
+		int delta_3s = 0;					// delta time between current and start time
+		int start_3s = micros();			// start 3 second current balancing
+		while(delta_3s <= 30000000)
+		{
+			delta_3s = micros() - start_3s; 				// delta time calculation 3 seconds
+			// if raw data is available 
+			MAX30100_sensor.update();		// Update sensor
+    		if (MAX30100_sensor.getRawValues(&raw_IR_Val, &raw_RED_Val))
+    		{
+				//Get DC value from IR signal:
+      			IR_DC = true;
+      			float IR_DC_val = DCR_function_IR(raw_IR_Val, ALPHA_DCR, IR_DC);       //Get DC value from IR signal
+      
+				//Get DC value from RED signal:
+      			RED_DC = true;
+				float RED_DC_val = DCR_function_RED(raw_RED_Val, ALPHA_DCR, RED_DC);    //Get DC value from RED signal
+				
+				//RED and IR DC current balancing:
+      			if (IR_DC_val - RED_DC_val > ACCEPTABLE_CURRENT_DIFF && RED_current < MAX30100_LED_CURR_50MA)
+      			{
+        			counter++;
+        			RED_current = LEDCurrent_array[counter];          			//change RED LED's current
+        			MAX30100_sensor.setLedsCurrent(IR_current, RED_current);   	//Set led's current IR and Red respectively
+        		}
+      			if (RED_DC_val - IR_DC_val > ACCEPTABLE_CURRENT_DIFF && RED_current > 0)
+     			{
+        			counter--;
+        			RED_current = LEDCurrent_array[counter];          			//change RED LED's current
+        			MAX30100_sensor.setLedsCurrent(IR_current, RED_current);   	//Set led's current IR and Red respectively
+        		}
+        		// Test print: 
+      			Serial.print(RED_DC_val); 
+      			Serial.print(" , ");
+      			Serial.println(IR_DC_val);
+			}
+		}
+		Current_balaning = false;			// Current balancing complete
+		MAX30100_sensor.shutdown();				// Shutdown MAX30100 sensor
+	}
   	//Every 10 seconds record 5's of HR and Sp02:
   	int time_10s_micro = micros();
   	if (time_10s_micro - time_10s >= 10000000)
   	{
   		time_10s = micros();		// redefine time
   		// insert code below:
-  		Serial.println("10 seconds");	// test print
+  		//Serial.println("10 seconds");	// test print
   		// while loop to record 5 seconds of data:
   		int delta_rec_5s = 0;				// delta time between current and start time
   		int start_rec_5s = micros();		// start record time 5 seconds
@@ -45,7 +142,7 @@ void loop()
   		{
   			delta_rec_5s = micros() - start_rec_5s; 				// delta time calculation 5 seconds
   		}
-  		Serial.println("exit while loop 5s");	// test print
+  		//Serial.println("exit while loop 5s");	// test print
   	}
 
   	//Every minute take a temperature reading:
@@ -54,7 +151,7 @@ void loop()
 	{
 		time_60s = micros();		// redefine time
 		//insert code below:
-		Serial.println("60 seconds");		// test print
+		//Serial.println("60 seconds");		// test print
 		//float Core_body_temp = temp_sensor.getObjectTemp();			// Get core body temperature
 		//Serial.println(Core_body_temp);								// print core body temperature ever 1 minute.
 	}
@@ -65,14 +162,110 @@ void loop()
   	{
   		time_300s = micros();		// redefine time
   		//insert code below:
-  		Serial.println("5 minute");	// test print
+  		//Serial.println("5 minute");	// test print
   		int delta_rec_30s = 0;			//delta time between current and start time
   		int start_rec_30s = micros();	// start record time 30 seconds
   		while(delta_rec_30s <= 30000000)
 		{
   		    delta_rec_30s = micros() - start_rec_30s;				// delta time calculation 30 seconds 
   		}
-  		Serial.println("exit while loop 30s");	// test print
+  		//Serial.println("exit while loop 30s");	// test print
   	}
 
 }
+
+//-------------------------MAX30100 START UP-----------------------------------------
+void MAX30100_Startup()
+{
+  //Serial.print("Initializing MAX30100..");
+  // Initialize the sensor
+  // Failures are generally due to an improper I2C wiring, missing power supply or wrong target chip
+  if (!MAX30100_sensor.begin()) {
+    Serial.println("FAILED");
+    for (;;);
+  } else {
+  //  Serial.println("SUCCESS");
+  }
+  //Serial.println("MAX30100 HR and SpO2 Sensor");
+  MAX30100_sensor.setMode(MAX30100_MODE_SPO2_HR);          //setting sensor mode (HR and SpO2)
+  MAX30100_sensor.setLedsCurrent(IR_current, RED_current); //Set led's current IR and Red respectively
+  MAX30100_sensor.setLedsPulseWidth(Pulse_width);          //Set led's pulse width
+  MAX30100_sensor.setSamplingRate(Sample_Rate);            //set sample rate
+  MAX30100_sensor.setHighresModeEnabled(Highres_mode);     //set high resolution
+  MAX30100_sensor.resetFifo();                             //rest fifo register 
+}
+//-------------------------Functions------------------------------------------------
+//DC Removeral filter for Heart Rate (HR)
+float DCR_function_IR(double raw_input, float alpha, bool ret) 
+{  
+  float output_DCR = 0;
+  float filtered = raw_input + alpha * prev_filtered_IR;
+  float AC_val = filtered - prev_filtered_IR;             //AC value of signal
+  float DC_val = filtered;                                //DC value of signal
+  //Weather to return the ac or dc value:
+  if (ret == true)
+  {
+    output_DCR = DC_val;
+  } 
+  else
+  {
+    prev_filtered_IR = filtered;
+    output_DCR = AC_val;
+  } 
+  //Serial.println(output_DCR); 
+  return output_DCR;
+}
+//DC Removeral filter for Sp02
+float DCR_function_RED(double raw_input, float alpha, bool ret) 
+{  
+  float output_DCR = 0;
+  float filtered = raw_input + alpha * prev_filtered_RED;
+  float AC_val = filtered - prev_filtered_RED;            //AC value of signal
+  float DC_val = filtered;                                //DC value of signal 
+  //Weather to return the ac or dc value:
+  if (ret == true)
+  {
+    output_DCR = DC_val;
+  } 
+  else
+  {
+    prev_filtered_RED = filtered; 
+    output_DCR = AC_val;
+  }
+  //Serial.println(output_DCR); 
+  return output_DCR;
+}
+// Mean difference filter
+float MDF_function(float raw_input) 
+{
+  float avg = 0; 
+  sum -= MeanDiff_TV[Index];
+  MeanDiff_TV[Index] = raw_input;
+  sum += MeanDiff_TV[Index];  
+  Index++;
+  Index = Index % SAMPLE_SIZE;
+  if (count < SAMPLE_SIZE) 
+  {
+    count++;
+  }
+  avg = sum / count;
+  //Serial.println(avg);
+  return avg - raw_input;
+}
+//Low pass filter (Butterworth filter)
+float Butterworth_LPF_function(float raw_input) 
+{
+  //Second order low pass filter
+  Val[0] = Val[1];
+  Val[1] = Val[2];
+  //Fs = 100Hz (sample rate) and Fc = 10Hz (cut-off frequency)
+  //Val[2] = (6.745527388907189559e-2 * raw_input) + (-0.41280159809618854894 * Val[0]) + (1.14298050253990091107 * Val[1]);
+  //Fs = 100Hz (sample rate) and Fc = 5Hz (cut-off frequency)
+  Val[2] = (2.008336556421122521e-2 * raw_input) + (-0.64135153805756306422 * Val[0]) + (1.56101807580071816339 * Val[1]);
+  //Fs = 100Hz (sample rate) and Fc = 3Hz (cut-off frequency)
+  //Val[2] = (7.820208033497201908e-3 * raw_input) + (-0.76600660094326400440 * Val[0]) + (1.73472576880927520371 * Val[1]);
+  float BWF_output = (Val[0] + Val[2]) + 2*Val[1];
+  //Serial.println(BWF_output);  
+  return BWF_output;
+}
+//----------------------------------------------------------------------------------
