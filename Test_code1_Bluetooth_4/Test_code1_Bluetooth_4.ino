38dc713236bafa44070378966fe119ddbc190f6d
jmburger <jarrydburger@gmail.com>
2018-07-11 12:59:15 +0200

    Testing Bluetooth
    Very simple works well send data to my phone using HM_10 bluetooth lite
    app

---
+++ Test_code1_Bluetooth_4/Test_code1_Bluetooth_4.ino
@@ -0,0 +1,10 @@
+void setup() 
+{
+  Serial1.begin(9600);  //Start communication with bluetooth module
+}
+
+void loop() 
+{
+  //Print to bluetooth serial monitor
+	Serial1.println("hi Jarryd");
+}
